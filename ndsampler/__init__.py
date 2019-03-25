dff992e7e19e75e0d0815fafa1f98b5430e4eeb0
joncrall <jon.crall@kitware.com>
2019-03-25 15:55:27 -0400

    Finalize version

--- ndsampler/__init__.py
+++ ndsampler/__init__.py
@@ -1,7 +1,7 @@
 """
 mkinit ~/code/ndsampler/ndsampler/__init__.py -w
 """
-__version__ = '0.1.0.dev0'
+__version__ = '0.1.0'
 from ndsampler import abstract_frames
 from ndsampler import abstract_sampler
 from ndsampler import category_tree
