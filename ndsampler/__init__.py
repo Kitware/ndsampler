6af66ddb94a068758d319d3359ceb472c82707e0
joncrall <jon.crall@kitware.com>
2020-02-03 14:01:51 -0500

    wip

--- ndsampler/__init__.py
+++ ndsampler/__init__.py
@@ -1,7 +1,7 @@
 """
 mkinit ~/code/ndsampler/ndsampler/__init__.py -w
 """
-__version__ = '0.5.3'
+__version__ = '0.5.4'
 from ndsampler import abstract_frames
 from ndsampler import abstract_sampler
 from ndsampler import category_tree
