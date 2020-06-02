ae837ad131bb08f5646cb03bf141abd3bcf4792d
joncrall <erotemic@gmail.com>
2020-06-02 08:17:55 -0400

    wip

--- ndsampler/__init__.py
+++ ndsampler/__init__.py
@@ -2,44 +2,63 @@
 mkinit ~/code/ndsampler/ndsampler/__init__.py -w
 """
 __version__ = '0.5.10'
+
+from ndsampler.utils.util_misc import (HashIdentifiable, stats_dict,)
+
+__explicit__ = [
+    'HashIdentifiable',
+    'stats_dict',
+]
+
+#  ---
+
+__submodules__ = [
+        'abstract_frames',
+        'abstract_sampler',
+        'category_tree',
+        'coco_frames',
+        'coco_regions',
+        'coco_sampler',
+        'isect_indexer',
+
+        # DEPRECATED (moved to kwcoco)
+        'toydata',
+        'toypatterns',
+        'coco_dataset',
+]
 from ndsampler import abstract_frames
 from ndsampler import abstract_sampler
 from ndsampler import category_tree
-from ndsampler import coco_dataset
 from ndsampler import coco_frames
 from ndsampler import coco_regions
 from ndsampler import coco_sampler
 from ndsampler import isect_indexer
 from ndsampler import toydata
 from ndsampler import toypatterns
+from ndsampler import coco_dataset
 
-from ndsampler.abstract_frames import (Frames, SimpleFrames,)
+from ndsampler.abstract_frames import (CorruptCOG, DEBUG_COG_ATOMIC_WRITE,
+                                       DEBUG_FILE_LOCK_CACHE_WRITE,
+                                       DEBUG_LOAD_COG, Frames,
+                                       RUN_COG_CORRUPTION_CHECKS,
+                                       SimpleFrames,)
 from ndsampler.abstract_sampler import (AbstractSampler,)
-from ndsampler.category_tree import (CategoryTree, entropy,
-                                     from_directed_nested_tuples, gini,
-                                     sink_nodes, source_nodes,
-                                     to_directed_nested_tuples,
-                                     traverse_siblings, tree_depth,)
-from ndsampler.coco_dataset import (CocoDataset,)
+from ndsampler.category_tree import (CategoryTree,)
 from ndsampler.coco_frames import (CocoFrames,)
 from ndsampler.coco_regions import (CocoRegions, MissingNegativePool, Targets,
                                     select_positive_regions,
                                     tabular_coco_targets,)
-from ndsampler.coco_sampler import (CocoSampler,)
+from ndsampler.coco_sampler import (CocoSampler, padded_slice,)
 from ndsampler.isect_indexer import (FrameIntersectionIndex,)
-from ndsampler.toydata import (DynamicToySampler, demodata_toy_dset,
-                               demodata_toy_img,)
-from ndsampler.toypatterns import (CategoryPatterns, Rasters, star,)
-from ndsampler.utils.util_misc import (HashIdentifiable, stats_dict,)
+from ndsampler.toydata import (DynamicToySampler,)
 
-__all__ = ['AbstractSampler', 'CategoryPatterns', 'CategoryTree',
-           'CocoDataset', 'CocoFrames', 'CocoRegions', 'CocoSampler',
+__all__ = ['AbstractSampler', 'CategoryTree', 'CocoFrames', 'CocoRegions',
+           'CocoSampler', 'CorruptCOG', 'DEBUG_COG_ATOMIC_WRITE',
+           'DEBUG_FILE_LOCK_CACHE_WRITE', 'DEBUG_LOAD_COG',
            'DynamicToySampler', 'FrameIntersectionIndex', 'Frames',
-           'HashIdentifiable', 'MissingNegativePool', 'Rasters',
-           'SimpleFrames', 'Targets', 'abstract_frames', 'abstract_sampler',
-           'category_tree', 'coco_dataset', 'coco_frames', 'coco_regions',
-           'coco_sampler', 'demodata_toy_dset', 'demodata_toy_img', 'entropy',
-           'from_directed_nested_tuples', 'gini', 'isect_indexer',
-           'select_positive_regions', 'sink_nodes', 'source_nodes', 'star',
-           'stats_dict', 'tabular_coco_targets', 'to_directed_nested_tuples',
-           'toydata', 'toypatterns', 'traverse_siblings', 'tree_depth']
+           'HashIdentifiable', 'MissingNegativePool',
+           'RUN_COG_CORRUPTION_CHECKS', 'SimpleFrames', 'Targets',
+           'abstract_frames', 'abstract_sampler', 'category_tree',
+           'coco_dataset', 'coco_frames', 'coco_regions', 'coco_sampler',
+           'isect_indexer', 'padded_slice', 'select_positive_regions',
+           'stats_dict', 'tabular_coco_targets', 'toydata', 'toypatterns']
