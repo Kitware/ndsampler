08be3b12255814f3da1df1c6b4a75878ea3a868c
joncrall <erotemic@gmail.com>
2022-01-11 15:16:10 -0500

    Remove 3.10 support for now

--- setup.py
+++ setup.py
@@ -157,5 +157,6 @@ if __name__ == '__main__':
             'Programming Language :: Python :: 3.7',
             'Programming Language :: Python :: 3.8',
             'Programming Language :: Python :: 3.9',
+            # 'Programming Language :: Python :: 3.10',  # need torch first
         ],
     )
