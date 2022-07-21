f6b9f74d267a00a1ab081274f030f322f19b769d
joncrall <erotemic@gmail.com>
2022-07-21 14:16:30 -0400

    Add demo CLI

--- examples/torch_dataloader.py
+++ examples/torch_dataloader.py
@@ -1,5 +1,7 @@
 """
-This demos how to get a speed improvement with ndsampler
+This demos how to get a speed improvement with ndsampler.
+
+On an NVMe drive I can get a 4x speedup on the default benchmark.
 """
 import torch
 import kwcoco
@@ -9,6 +11,41 @@ import ndsampler
 from torch.utils import data as torch_data
 import numpy as np
 import ubelt as ub
+import scriptconfig as scfg
+
+
+@scfg.dataconf
+class DemoConfig:
+    """
+    This is the default configuration for the demo. You can modify it with
+    command line parameters to change things like the number of images, image
+    sizes, and the path to where the demo happens (HDDs will perform
+    differently than SSDs! This filesystem also matters!)
+    """
+    dpath = scfg.Value(None, help=ub.paragraph(
+        '''
+        The path to where the demo writes its data.
+        Defaults to the ndsmapler application cache directory
+        '''))
+
+    image_format = scfg.Value('png', choices=['png', 'jpg', 'cog'], help=ub.paragraph(
+        '''
+        The format the initial demo images will be written as.
+        This will impact multiple parts of the benchmark, particularly
+        initial cache generation.
+        '''))
+
+    num_images = scfg.Value(32, help='number of demo images in the dataset')
+
+    image_width = scfg.Value(1920, help='The demo image width')
+    image_height = scfg.Value(1080, help='The demo image height')
+
+    num_passes = scfg.Value(10, help=ub.paragraph(
+        '''
+        Number of dataloading passes to perform. The first COG backend
+        pass will be slower as ndsampler builds its data structures, but
+        the rest will faster.
+        '''))
 
 
 class DemoDataset(torch_data.Dataset):
@@ -54,28 +91,42 @@ class DemoDataset(torch_data.Dataset):
 
 
 def demo():
-    dpath = ub.Path.appdir('ndsampler/demo/torch_dataloader').ensuredir()
-    image_dpath = dpath / 'images'
+    config = DemoConfig.cli()
+    if config['dpath'] is None:
+        config['dpath'] = dpath = ub.Path.appdir('ndsampler/demo/torch_dataloader')
+    else:
+        dpath = ub.Path(config['dpath'])
+
+    print('config = {}'.format(ub.repr2(config.asdict(), nl=1)))
+
+    dpath.ensuredir()
     workdir = dpath / 'workdir'
 
-    num_passes = 10
+    num_passes = config['num_passes']
 
+    # We could seed the rng if we wanted to.
     rng = None
     rng = kwarray.ensure_rng(rng)
-
     demo_image_params = {
-        'num_images': 64,
-        'image_dsize': (1920, 1080),
+        'num_images': config['num_images'],
+        'image_dsize': (config['image_width'], config['image_height']),
+        'image_format': config['image_format'],
     }
+    image_dpath = dpath / 'images_{image_format}'.format(**demo_image_params)
     stamp = ub.CacheStamp('demo_images', dpath=dpath, depends=demo_image_params)
     if stamp.expired():
+        ext = config['image_format']
+        imwrite_backend = 'auto'
+        if ext == 'cog':
+            ext = 'tif'
+            imwrite_backend = 'gdal'
         image_dpath.ensuredir()
         w, h = demo_image_params['image_dsize']
         for image_idx in ub.ProgIter(range(demo_image_params['num_images']), desc='write demo images'):
             imdata = kwimage.ensure_uint255(rng.rand(h, w, 3))
-            image_name = f'demo_image_{image_idx:03d}.png'
+            image_name = f'demo_image_{image_idx:03d}.{ext}'
             gpath = image_dpath / image_name
-            kwimage.imwrite(gpath, imdata)
+            kwimage.imwrite(gpath, imdata, backend=imwrite_backend)
         stamp.renew()
 
     print('')
@@ -83,9 +134,10 @@ def demo():
     print('Test ndsampler with COG backend')
     print('Note the first time this is run, there is a conversion to COG '
           'which takes some time. Running a second time will be much faster')
-    torch_dset = DemoDataset(image_dpath, backend='cog', workdir=workdir)
+    with ub.Timer('Sampler Construction, backend=COG') as cog_construct:
+        torch_dset = DemoDataset(image_dpath, backend='cog', workdir=workdir)
     loader = torch_data.DataLoader(torch_dset, batch_size=1)
-    with ub.Timer('With COG') as cog_timer:
+    with ub.Timer('Load Data, backend=COG') as cog_timer:
         # Make N passes through the dataloader
         for _ in range(num_passes):
             for item in ub.ProgIter(loader, desc='loading data'):
@@ -94,8 +146,9 @@ def demo():
     print('')
     print('===============================')
     print('Test ndsampler without COG backend')
-    torch_dset = DemoDataset(image_dpath, backend=None)
-    with ub.Timer('Without COG') as default_timer:
+    with ub.Timer('Sampler Construction, backend=None') as nocog_construct:
+        torch_dset = DemoDataset(image_dpath, backend=None)
+    with ub.Timer('Load Data, backend=None') as nocog_timer:
         # Make N passes through the dataloader
         for _ in range(num_passes):
             loader = torch_data.DataLoader(torch_dset, batch_size=1)
@@ -106,8 +159,10 @@ def demo():
     print('===============================')
     print('Final Results')
     print('===============================')
-    print(f'default_timer.elapsed = {default_timer.elapsed}')
-    print(f'cog_timer.elapsed     = {cog_timer.elapsed}')
+    print(f'nocog_construct.elapsed = {nocog_construct.elapsed}')
+    print(f'cog_construct.elapsed   = {cog_construct.elapsed}')
+    print(f'nocog_timer.elapsed = {nocog_timer.elapsed}')
+    print(f'cog_timer.elapsed   = {cog_timer.elapsed}')
 
 
 if __name__ == '__main__':
