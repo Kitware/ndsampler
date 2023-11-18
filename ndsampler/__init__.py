4603c27dfb4251827405683d56b8d812a4aebb76
joncrall <erotemic@gmail.com>
2023-11-17 22:01:34 -0500

    Start branch for 0.7.8

--- ndsampler/__init__.py
+++ ndsampler/__init__.py
@@ -2,7 +2,7 @@
 mkinit ~/code/ndsampler/ndsampler/__init__.py --diff
 mkinit ~/code/ndsampler/ndsampler/__init__.py -w
 """
-__version__ = '0.7.7'
+__version__ = '0.7.8'
 
 from ndsampler.utils.util_misc import (HashIdentifiable,)
 
